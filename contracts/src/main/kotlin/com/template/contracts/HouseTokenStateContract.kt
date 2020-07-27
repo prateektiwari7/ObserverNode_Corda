dfe2e349fcfbbeb4d09afea2d7aa96f140d333ef
Prateek  Tiwari <prateek.tiwari456@gmail.com>
2020-07-27 18:26:52 +0530

    Tokenstate observer

---
+++ contracts/src/main/kotlin/com/template/contracts/HouseTokenStateContract.kt
@@ -0,0 +1,17 @@
+package com.template.contracts
+
+import com.r3.corda.lib.tokens.contracts.EvolvableTokenContract
+import net.corda.core.contracts.Contract
+import net.corda.core.transactions.LedgerTransaction
+
+// ************
+// * Contract *
+// ************
+class HouseTokenStateContract : EvolvableTokenContract(), Contract {
+    override fun additionalCreateChecks(tx: LedgerTransaction) {
+    }
+
+    override fun additionalUpdateChecks(tx: LedgerTransaction) {
+    }
+
+}
